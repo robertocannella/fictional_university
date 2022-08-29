f0ffc5f0270bd6359495eb8a2b30e96c9c83e4aa
robertocannella <robertocannella@gmail.com>
2022-08-28 20:18:16 -0400

    Add dynamic nav menus

--- wp-content/themes/fictional_university_theme/header.php
+++ wp-content/themes/fictional_university_theme/header.php
@@ -24,19 +24,20 @@
         <div class="site-header__menu group">
             <nav class="main-navigation">
                 <?php
-                wp_nav_menu([
-                    'theme_location'=>'headerMenuLocation1'
-                ]);
-                ?>
+// This implementation for dynamic menu through customizer
+//                wp_nav_menu([
+//                    'theme_location'=>'headerMenuLocation1'
+//                ]);
+//                ?>
 
-<!--                 <ul>
-                    <li><a href="<?php echo site_url('/about-us');?>">About Us</a></li>
-                    <li><a href="#">Programs</a></li>
-                    <li><a href="#">Events</a></li>
-                    <li><a href="#">Campuses</a></li>
-                    <li><a href="#">Blog</a></li>
+                 <ul>
+                    <li <?php if (is_page('about-us') || wp_get_post_parent_id(0)== 8) echo 'class="current-menu-item"'?>><a href="<?php echo site_url('/about-us');?>">About Us</a></li>
+                    <li <?php if (is_page('programs') || wp_get_post_parent_id(0)== 8) echo 'class="current-menu-item"'?>><a href="#">Programs</a></li>
+                    <li <?php if (is_page('events') || wp_get_post_parent_id(0)== 8) echo 'class="current-menu-item"'?>><a href="#">Events</a></li>
+                    <li <?php if (is_page('campuses') || wp_get_post_parent_id(0)== 8) echo 'class="current-menu-item"'?>><a href="#">Campuses</a></li>
+                    <li <?php if (is_page('blog') || wp_get_post_parent_id(0)== 8) echo 'class="current-menu-item"'?>><a href="#">Blog</a></li>
                 </ul>
--->
+
             </nav>
             <div class="site-header__util">
                 <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
