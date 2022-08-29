9d58995d625fe5d8974749403e6cb4884e0a57e1
robertocannella <robertocannella@gmail.com>
2022-08-29 11:50:37 -0400

    Add dynamic events to front-page.php

--- wp-content/themes/fictional_university_theme/front-page.php
+++ wp-content/themes/fictional_university_theme/front-page.php
@@ -23,15 +23,33 @@ get_header(); ?>
             <div class="full-width-split__inner">
                 <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
                     <?php
+                    $today = date('Ymd');
                     $homePageEvents = new WP_Query([
-                            'posts_per_page'=>2,
-                            'post_type'=>'event'
+                            'posts_per_page'=>-1,
+                            'post_type'=>'event',
+                            //'orderby'=> 'title',
+                            'order'=>'ASC',
+                            'orderby'=>'meta_value_num', // meta_key required
+                            'meta_key'=>'event_date',
+                            'meta_query'=>[
+                                    [
+                                        'key'=>'event_date',
+                                        'compare'=>'>=',
+                                        'value'=>$today,
+                                        'type'=>'numeric'
+                                    ]
+                            ]
                     ]);
                     while ($homePageEvents->have_posts()): $homePageEvents->the_post(); ?>
                         <div class="event-summary">
-                            <a class="event-summary__date t-center" href="#">
-                                <span class="event-summary__month">Apr</span>
-                                <span class="event-summary__day">02</span>
+                            <a class="event-summary__date t-center" href="<?php the_permalink();?>">
+                                <span class="event-summary__month">
+                                    <?php
+                                    $eventDate = new DateTime(get_field('event_date'));
+                                    echo $eventDate->format('M');
+                                    ?>
+                                </span>
+                                <span class="event-summary__day"><?php echo $eventDate->format('d');?></span>
                             </a>
                             <div class="event-summary__content">
                                 <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
